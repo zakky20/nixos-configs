9db264aebf932db6179d462757f5dd40a02ae52a
Zakky <150591719+zakky20@users.noreply.github.com>
2025-10-14 11:28:08 +0300

    Update yazi.nix

--- modules/home/apps/yazi.nix
+++ modules/home/apps/yazi.nix
@@ -4,16 +4,20 @@
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+
     settings = {
+
       mgr = {
         ratio = [2 4 3];
         sort_by = "alphabetical";
         show_symlink = true;
       };
+
       preview = {
         max_width = 1920;
         max_height = 1080;
       };
-    };
+    
+     };
   };
 }

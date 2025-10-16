6c91ac0f1085ab0f651d8a412e2b2049f7f29202
Zakky <150591719+zakky20@users.noreply.github.com>
2025-10-16 09:53:08 +0300

    Update eza.nix

--- modules/home/shell/eza.nix
+++ modules/home/shell/eza.nix
@@ -2,6 +2,13 @@
 
   programs.eza = {
     enable = true;
+    enableZshIntegration = true;
+    colors = "always";
+    git = true;
+    icons = "always";
+    extraOptions = [
+      "--group-directories-first"
+      "--header"
+    ];
   };
-
 }

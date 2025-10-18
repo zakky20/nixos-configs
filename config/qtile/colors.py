dcf7a4a261341b32f42936c228f735b7b9eeb9b9
Zakky <150591719+zakky20@users.noreply.github.com>
2025-10-18 17:56:13 +0300

    Add files via upload

---
+++ config/qtile/colors.py
@@ -0,0 +1,145 @@
+Tokyonight = [
+    ["#1a1b26", "#1a1b26"], # color[0] - background
+    ["#c0caf5", "#c0caf5"], # color[2] - foreground
+    ["#24283b", "#24283b"], # color[3] - dark background
+    ["#f7768e", "#f7768e"], # color[4] - red
+    ["#9ece6a", "#9ece6a"], # color[5] - green
+    ["#e0af68", "#e0af68"], # color[6] - yellow
+    ["#7aa2f7", "#7aa2f7"], # color[7] - blue
+    ["#bb9af7", "#bb9af7"], # color[8] - purple
+    ["#7dcfff", "#7dcfff"], # color[9] - cyan
+    ["#414868", "#414868"], # dark gray
+    ["#565f89", "#565f89"], # medium gray
+    ["#787c99", "#787c99"]  # color[10] - light gray
+]
+
+Catppuccin = [
+    ["#1e1e2e", "#1e1e2e"], # color[0] - base
+    ["#cdd6f4", "#cdd6f4"], # color[2] - text
+    ["#313244", "#313244"], # color[3] - surface0
+    ["#f38ba8", "#f38ba8"], # color[4] - red
+    ["#a6e3a1", "#a6e3a1"], # color[5] - green
+    ["#f9e2af", "#f9e2af"], # color[6] - yellow
+    ["#89b4fa", "#89b4fa"], # color[7] - blue
+    ["#cba6f7", "#cba6f7"], # color[8] - mauve
+    ["#89dceb", "#89dceb"], # color[9] - sky
+    ["#45475a", "#45475a"], # surface1
+    ["#585b70", "#585b70"], # surface2
+    ["#6c7086", "#6c7086"]  # color[10] - overlay0
+]
+
+OneDark = [
+    ["#282c34", "#282c34"], # color[0] - background
+    ["#abb2bf", "#abb2bf"], # color[2] - foreground
+    ["#21252b", "#21252b"], # color[3] - dark background
+    ["#e06c75", "#e06c75"], # color[4] - red
+    ["#98c379", "#98c379"], # color[5] - green
+    ["#e5c07b", "#e5c07b"], # color[6] - yellow
+    ["#61afef", "#61afef"], # color[7] - blue
+    ["#c678dd", "#c678dd"], # color[8] - purple
+    ["#56b6c2", "#56b6c2"], # color[9] - cyan
+    ["#3e4451", "#3e4451"], # dark gray
+    ["#4b5263", "#4b5263"], # medium gray
+    ["#5c6370", "#5c6370"]  # color[10] - light gray
+]
+
+Solarized = [
+    ["#002b36", "#002b36"], # color[0] - background
+    ["#839496", "#839496"], # color[2] - foreground
+    ["#073642", "#073642"], # color[3] - dark background
+    ["#dc322f", "#dc322f"], # color[4] - red
+    ["#859900", "#859900"], # color[5] - green
+    ["#b58900", "#b58900"], # color[6] - yellow
+    ["#268bd2", "#268bd2"], # color[7] - blue
+    ["#6c71c4", "#6c71c4"], # color[8] - violet
+    ["#2aa198", "#2aa198"], # color[9] - cyan
+    ["#586e75", "#586e75"], # dark gray
+    ["#657b83", "#657b83"], # medium gray
+    ["#93a1a1", "#93a1a1"]  # color[10] - light gray
+]
+
+Kanagawa = [
+    ["#1f1f28", "#1f1f28"],  # bg (sumi ink)
+    ["#dcd7ba", "#dcd7ba"],  # fg (fuji white)
+    ["#2a2a37", "#2a2a37"],  # dark gray (wave blue 2)
+    ["#c34043", "#c34043"],  # red (samurai red)
+    ["#76946a", "#76946a"],  # green (autumn green)
+    ["#c0a36e", "#c0a36e"],  # yellow (koi carp yellow)
+    ["#7e9cd8", "#7e9cd8"],  # blue (dragon blue)
+    ["#957fb8", "#957fb8"],  # purple (oni purple)
+    ["#6a9589", "#6a9589"],  # cyan (wave aqua 2)
+    ["#727169", "#727169"]   # gray (fuji gray)
+]
+
+Oxocarbon = [
+    ["#161616", "#161616"], # color[0] - base00
+    ["#f2f4f8", "#f2f4f8"], # color[1] - base05
+    ["#262626", "#262626"], # color[2] - base01
+    ["#393939", "#393939"], # color[3] - base02
+    ["#ee5396", "#ee5396"], # color[4] - base08
+    ["#42be65", "#42be65"], # color[5] - base0B
+    ["#ff7eb6", "#ff7eb6"], # color[6] - base0A
+    ["#33b1ff", "#33b1ff"], # color[7] - base0D
+    ["#be95ff", "#be95ff"], # color[8] - base0E
+    ["#08bdba", "#08bdba"], # color[9] - base0C
+    ["#525252", "#525252"], # color[10] - base03
+    ["#dde1e6", "#dde1e6"]  # color[11] - base04
+]
+
+Nord = [
+    ["#242933", "#242933"], # color[0] - base00
+    ["#d8dee9", "#d8dee9"], # color[1] - base04
+    ["#3b4252", "#3b4252"], # color[2] - base02
+    ["#bf616a", "#bf616a"], # color[3] - base11
+    ["#a3be8c", "#a3be8c"], # color[4] - base14
+    ["#ebcb8b", "#ebcb8b"], # color[5] - base13
+    ["#81a1c1", "#81a1c1"], # color[6] - base09
+    ["#b48ead", "#b48ead"], # color[7] - base15
+    ["#88c0d0", "#88c0d0"], # color[8] - base08
+    ["#434c5e", "#434c5e"], # color[9] - base03
+    ["#2e3440", "#2e3440"], # color[10] - base01
+    ["#e5e9f0", "#e5e9f0"]  # color[11] - base05
+]
+
+Everforest = [
+    ["#2d353b", "#2d353b"], # color[0] - dark background
+    ["#d3c6aa", "#d3c6aa"], # color[2] - light foreground
+    ["#343f44", "#343f44"], # color[3] - dark1
+    ["#e67e80", "#e67e80"], # color[4] - red
+    ["#a7c080", "#a7c080"], # color[5] - green
+    ["#dbbc7f", "#dbbc7f"], # color[6] - yellow
+    ["#7fbbb3", "#7fbbb3"], # color[7] - blue
+    ["#d699b6", "#d699b6"], # color[8] - purple
+    ["#83c092", "#83c092"], # color[9] - aqua
+    ["#3d484d", "#3d484d"], # dark2
+    ["#475258", "#475258"], # dark3
+    ["#859289", "#859289"]  # color[10] - gray
+]
+
+Gruvbox = [
+    ["#282828", "#282828"], # color[0] - dark background
+    ["#ebdbb2", "#ebdbb2"], # color[2] - light foreground
+    ["#3c3836", "#3c3836"], # color[3] - dark1
+    ["#cc241d", "#cc241d"], # color[4] - red
+    ["#98971a", "#98971a"], # color[5] - green
+    ["#d79921", "#d79921"], # color[6] - yellow
+    ["#458588", "#458588"], # color[7] - blue
+    ["#b16286", "#b16286"], # color[8] - purple
+    ["#689d6a", "#689d6a"], # color[9] - aqua
+    ["#504945", "#504945"], # dark2
+    ["#665c54", "#665c54"], # dark3
+    ["#928374", "#928374"]  # color[10] - gray
+]
+
+Rosepine = [
+    ["#191724", "#191724"],  # bg (background)
+    ["#e0def4", "#e0def4"],  # fg (foreground)
+    ["#1f1d2e", "#1f1d2e"],  # color01 (dark shade, alt bg)
+    ["#eb6f92", "#eb6f92"],  # color02 (red/rose)
+    ["#31748f", "#31748f"],  # color03 (pine/teal)
+    ["#f6c177", "#f6c177"],  # color04 (gold/yellow)
+    ["#9ccfd8", "#9ccfd8"],  # color05 (foam/blue)
+    ["#c4a7e7", "#c4a7e7"],  # color06 (iris/purple)
+    ["#ebbcba", "#ebbcba"],  # color15 (love/pinkish accent)
+    ["#6e6a86", "#6e6a86"],  # color09 (gray/subtle text)
+]

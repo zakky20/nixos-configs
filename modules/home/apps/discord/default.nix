15dcf9403c6c7a69d3ebfe55d6d628f1aededf12
Zakky <150591719+zakky20@users.noreply.github.com>
2025-10-18 19:39:04 +0300

    Update default.nix

--- modules/home/apps/discord/default.nix
+++ modules/home/apps/discord/default.nix
@@ -1,29 +1,5 @@
 { pkgs, ... }:
 
-let
-  selectedTheme = {
-    colors = {
-      base00 = "#242933";
-      base01 = "#2e3440";
-      base02 = "#3b4252";
-      base03 = "#434c5e";
-      base04 = "#d8dee9";
-      base05 = "#e5e9f0";
-      base06 = "#eceff4";
-      base07 = "#8fbcbb";
-      base08 = "#88c0d0";
-      base09 = "#81a1c1";
-      base10 = "#5e81ac";
-      base11 = "#bf616a";
-      base12 = "#d08770";
-      base13 = "#ebcb8b";
-      base14 = "#a3be8c";
-      base15 = "#b48ead";
-    };
-  };
-  
-  font = "Iosevka Nerd Font";
-in
 {
   imports = [./settings.nix];
   
@@ -37,140 +13,123 @@ in
     @import url('https://refact0r.github.io/midnight-discord/build/midnight.css');
 
     body {
-        --font: '${font}';
+      --font: 'Iosevka Nerd Font';
+      --code-font: ${"''"};
+      font-weight: 400;
 
-        --gap: 12px;
-        --divider-thickness: 4px;
+      --gap: 12px;
+      --divider-thickness: 4px;
+      --border-thickness: 1px;
 
-        --animations: on;
-        --list-item-transition: 0.2s ease;
-        --dms-icon-svg-transition: 0.4s ease;
+      --animations: on;
+      --list-item-transition: 0.2s ease;
+      --dms-icon-svg-transition: 0.4s ease;
+      --border-hover-transition: 0.2s ease;
 
-        --move-top-bar-buttons: off;
-        --custom-app-top-bar-height: 32px;
+      --top-bar-height: var(--gap);
+      --top-bar-button-position: titlebar;
+      --top-bar-title-position: off;
+      --subtle-top-bar-title: off;
 
-        --custom-window-controls: on;
-        --window-control-size: 14px;
+      --custom-window-controls: on;
+      --window-control-size: 14px;
 
-        --dms-icon: on;
-        --dms-icon-svg-url: url('https://upload.wikimedia.org/wikipedia/commons/c/c4/Font_Awesome_5_solid_moon.svg');
-        --dms-icon-svg-size: 90%;
-        --dms-icon-color-before: var(--icon-secondary);
-        --dms-icon-color-after: var(--white);
+      --custom-dms-icon: custom;
+      --dms-icon-svg-url: url('https://refact0r.github.io/midnight-discord/assets/Font_Awesome_5_solid_moon.svg');
+      --dms-icon-svg-size: 90%;
+      --dms-icon-color-before: var(--icon-secondary);
+      --dms-icon-color-after: var(--white);
+      --custom-dms-background: off;
+      --dms-background-image-url: url(${"''"});
+      --dms-background-image-size: cover;
+      --dms-background-color: linear-gradient(70deg, var(--blue-2), var(--purple-2), var(--red-2));
 
-        --dms-background: off;
-        --dms-background-image-url: url("");
-        --dms-background-image-size: cover;
-        --dms-background-color: linear-gradient(70deg, ${selectedTheme.colors.base08}, ${selectedTheme.colors.base15}, ${selectedTheme.colors.base11});
+      --background-image: off;
+      --background-image-url: url(${"''"});
 
-        --background-image: off;
-        --background-image-url: url("");
+      --transparency-tweaks: off;
+      --remove-bg-layer: off;
+      --panel-blur: off;
+      --blur-amount: 12px;
+      --bg-floating: var(--bg-3);
 
-        --transparency-tweaks: off;
-        --remove-bg-layer: off;
-        --panel-blur: off;
-        --blur-amount: 12px;
-        --bg-floating: ${selectedTheme.colors.base00};
+      --custom-chatbar: aligned;
+      --chatbar-height: 47px;
+      --chatbar-padding: 8px;
 
-        --flipped-chatbar: on;
-        --chatbar-height: 47px;
-        --chatbar-padding: 8px;
-        --small-user-panel: off;
+      --small-user-panel: on;
     }
 
     :root {
-        --colors: on;
+      --colors: on;
 
-        --text-0: ${selectedTheme.colors.base00};
-        --text-1: ${selectedTheme.colors.base06};
-        --text-2: ${selectedTheme.colors.base05};
-        --text-3: ${selectedTheme.colors.base05};
-        --text-4: ${selectedTheme.colors.base04};
-        --text-5: ${selectedTheme.colors.base03};
+      --text-0: var(--bg-3);
+      --text-1: #eceff4;
+      --text-2: #e5e9f0;
+      --text-3: #d8dee9;
+      --text-4: hsl(220, 20%, 67%);
+      --text-5: #4c566a;
 
-        --bg-1: ${selectedTheme.colors.base03};
-        --bg-2: ${selectedTheme.colors.base02};
-        --bg-3: ${selectedTheme.colors.base00};
-        --bg-4: ${selectedTheme.colors.base01};
+      --bg-1: #434c5e;
+      --bg-2: #3b4252;
+      --bg-3: #272b35;
+      --bg-4: #2e3440;
+      --hover: hsla(220, 20%, 50%, 0.1);
+      --active: hsla(220, 20%, 50%, 0.2);
+      --active-2: hsla(220, 20%, 50%, 0.3);
+      --message-hover: hsla(0, 0%, 0%, 0.1);
 
-        --hover: color-mix(in srgb, ${selectedTheme.colors.base06} 5%, transparent);
-        --active: color-mix(in srgb, ${selectedTheme.colors.base06} 8%, transparent);
-        --active-2: color-mix(in srgb, ${selectedTheme.colors.base06} 12%, transparent);
-        --message-hover: color-mix(in srgb, ${selectedTheme.colors.base06} 3%, transparent);
+      --accent-1: var(--blue-1);
+      --accent-2: var(--blue-2);
+      --accent-3: var(--blue-3);
+      --accent-4: var(--blue-4);
+      --accent-5: var(--blue-5);
+      --accent-new: var(--accent-2);
+      --mention: linear-gradient(to right, color-mix(in hsl, var(--accent-2), transparent 90%) 40%, transparent);
+      --mention-hover: linear-gradient(to right, color-mix(in hsl, var(--accent-2), transparent 95%) 40%, transparent);
+      --reply: linear-gradient(to right, color-mix(in hsl, var(--text-3), transparent 90%) 40%, transparent);
+      --reply-hover: linear-gradient(to right, color-mix(in hsl, var(--text-3), transparent 95%) 40%, transparent);
 
-        --accent-1: ${selectedTheme.colors.base08};
-        --accent-2: ${selectedTheme.colors.base11};
-        --accent-3: ${selectedTheme.colors.base12};
-        --accent-4: ${selectedTheme.colors.base13};
-        --accent-5: ${selectedTheme.colors.base14};
-        --accent-new: ${selectedTheme.colors.base09};
+      --online: var(--green-2);
+      --dnd: var(--red-2);
+      --idle: var(--yellow-2);
+      --streaming: var(--purple-2);
+      --offline: var(--text-4);
 
-        --mention: linear-gradient(to right, color-mix(in srgb, ${selectedTheme.colors.base11}, transparent 90%) 40%, transparent);
-        --mention-hover: linear-gradient(to right, color-mix(in srgb, ${selectedTheme.colors.base11}, transparent 95%) 40%, transparent);
-        --reply: linear-gradient(to right, color-mix(in srgb, ${selectedTheme.colors.base04}, transparent 90%) 40%, transparent);
-        --reply-hover: linear-gradient(to right, color-mix(in srgb, ${selectedTheme.colors.base04}, transparent 95%) 40%, transparent);
+      --border-light: hsla(220, 20%, 50%, 0.05);
+      --border: hsla(220, 20%, 50%, 0.1);
+      --border-hover: hsla(220, 20%, 50%, 0.1);
+      --button-border: hsl(0, 0%, 100%, 0.1);
 
-        --online: ${selectedTheme.colors.base14};
-        --dnd: ${selectedTheme.colors.base11};
-        --idle: ${selectedTheme.colors.base10};
-        --streaming: ${selectedTheme.colors.base13};
-        --offline: ${selectedTheme.colors.base03};
+      --red-1: hsl(354, 42%, 61%);
+      --red-2: #bf616a;
+      --red-3: #bf616a;
+      --red-4: hsl(354, 42%, 51%);
+      --red-5: hsl(354, 42%, 46%);
 
-        --border-light: color-mix(in srgb, ${selectedTheme.colors.base06} 5%, transparent);
-        --border: color-mix(in srgb, ${selectedTheme.colors.base06} 10%, transparent);
-        --button-border: color-mix(in srgb, ${selectedTheme.colors.base06} 10%, transparent);
+      --green-1: hsl(92, 28%, 70%);
+      --green-2: #a3be8c;
+      --green-3: #a3be8c;
+      --green-4: hsl(92, 28%, 60%);
+      --green-5: hsl(92, 28%, 55%);
 
-        --red-1: ${selectedTheme.colors.base08};
-        --red-2: ${selectedTheme.colors.base08};
-        --red-3: ${selectedTheme.colors.base08};
-        --red-4: ${selectedTheme.colors.base08};
-        --red-5: ${selectedTheme.colors.base08};
+      --blue-1: hsl(193, 43%, 72%);
+      --blue-2: #88c0d0;
+      --blue-3: #88c0d0;
+      --blue-4: hsl(193, 43%, 62%);
+      --blue-5: hsl(193, 43%, 57%);
 
-        --green-1: ${selectedTheme.colors.base11};
-        --green-2: ${selectedTheme.colors.base11};
-        --green-3: ${selectedTheme.colors.base11};
-        --green-4: ${selectedTheme.colors.base11};
-        --green-5: ${selectedTheme.colors.base11};
+      --yellow-1: hsl(40, 71%, 78%);
+      --yellow-2: #ebcb8b;
+      --yellow-3: #ebcb8b;
+      --yellow-4: hsl(40, 71%, 68%);
+      --yellow-5: hsl(40, 71%, 63%);
 
-        --blue-1: ${selectedTheme.colors.base13};
-        --blue-2: ${selectedTheme.colors.base13};
-        --blue-3: ${selectedTheme.colors.base13};
-        --blue-4: ${selectedTheme.colors.base13};
-        --blue-5: ${selectedTheme.colors.base13};
-
-        --yellow-1: ${selectedTheme.colors.base10};
-        --yellow-2: ${selectedTheme.colors.base10};
-        --yellow-3: ${selectedTheme.colors.base10};
-        --yellow-4: ${selectedTheme.colors.base10};
-        --yellow-5: ${selectedTheme.colors.base10};
-
-        --orange-1: ${selectedTheme.colors.base09};
-        --orange-2: ${selectedTheme.colors.base09};
-        --orange-3: ${selectedTheme.colors.base09};
-        --orange-4: ${selectedTheme.colors.base09};
-        --orange-5: ${selectedTheme.colors.base09};
-
-        --purple-1: ${selectedTheme.colors.base14};
-        --purple-2: ${selectedTheme.colors.base14};
-        --purple-3: ${selectedTheme.colors.base14};
-        --purple-4: ${selectedTheme.colors.base14};
-        --purple-5: ${selectedTheme.colors.base14};
-
-        /* Bind internal Discord variables to our dynamic theme */
-        --text-muted: var(--text-3);
-        --text-normal: var(--text-2);
-        --channels-default: var(--text-2);
-        --interactive-normal: var(--text-3);
-        --interactive-hover: var(--text-2);
-        --interactive-active: var(--accent-2);
-        --header-primary: var(--text-1);
-        --header-secondary: var(--text-3);
-        --text-link: var(--blue-2);
-        --info-warning-foreground: var(--yellow-2);
-        --info-danger-foreground: var(--red-2);
-        --status-danger: var(--red-2);
-        --status-warning: var(--yellow-2);
-        --status-positive: var(--green-2);
+      --purple-1: hsl(311, 20%, 68%);
+      --purple-2: #b48ead;
+      --purple-3: #b48ead;
+      --purple-4: hsl(311, 20%, 58%);
+      --purple-5: hsl(311, 20%, 53%);
     }
   '';
 }

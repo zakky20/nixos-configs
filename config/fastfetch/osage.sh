75f5091f9730316fa69dd1623d25f16212f2ab22
Zakky <150591719+zakky20@users.noreply.github.com>
2025-10-18 15:46:46 +0300

    Delete config directory

--- config/fastfetch/osage.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-birth_install=$(stat -c %W /)
-current=$(date +%s)
-time_progression=$((current - birth_install))
-days_difference=$((time_progression / 86400))
-echo $days_difference days

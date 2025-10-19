6680ac53b4126e7a43c26907edc8296c1b6041ff
Zakky <150591719+zakky20@users.noreply.github.com>
2025-10-19 19:38:25 +0300

    Update git.nix

--- modules/home/apps/git.nix
+++ modules/home/apps/git.nix
@@ -2,17 +2,22 @@
 
 programs.git = {
     enable = true;
+
     settings.user.name = "zakky20";
     settings.user.email = "niku.raul@gmail.com";
+
     settings = {
       credential.helper = "store";
       init.defaultBranch = "main";
+
       core = {
         editor = "nvim";
         autocrlf = "input";
       };
+
       pull.rebase = true;
       push.autoSetupRemote = true;
+
     };
   };
 } 
